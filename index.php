cfadff39e4a5aa67209d4d5b8ce47a69b4fd93ee
notrealinqx <40286042+notrealinqx@users.noreply.github.com>
2018-07-04 13:36:05 +0700

    lol

--- index.php
+++ index.php
@@ -8,8 +8,16 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = 'elrTlEnZYv9BqQTLFDG+PsaT3VdBjCzs9/nhqkNNGFaHQDveBfVE2xL0ddW+PGl1sK/tCikVIoIq8ZcPaPIkgNIWdRO/QeEEENO0+UzmaKZrcZbCc9DDQ8cyoNuVN3Z0R4ewRaMjlDmMD3rePRDxnQdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = '47bc90719fa07a6a119bea4d462a29f6'; 
 
-// Get message from Line API 
+$getData = json_decode(file_get_contents('http://192.168.10.241:5000/api/fromDB'), TRUE);
 
+if(!empty($getData['DB'])) {
+    $priceList = [];
+    foreach($getData['DB'] as $val) {
+        $priceList[$val['symbol']] = 'Price: ' . $val['quotes']['USD']['price'] . 'USD ' .$val['quotes']['THB']['price'] . ' THB'; 
+    }
+}
+
+// Get message from Line API 
 $content = file_get_contents('php://input');
 $events = json_decode($content, true); 
 
@@ -24,7 +32,12 @@ if (!is_null($events['events'])) {
               
       switch($event['message']['type']) { 
             case 'text': 
-                $respMessage = 'Hello, your message is '. $event['message']['text'];
+              if(in_array( strtoupper($event['message']['text']), $priceList )) {
+                  $respMessage = $priceList[strtoupper($event['message']['text'])];
+              }else {
+                  $respMessage = 'Hello, your message is '. $event['message']['text'];
+              }
+//                $respMessage = 'Hello, your message is '. $event['message']['text'];
               //  $respMessage = getdata();
                 break;
                 default:
@@ -48,23 +61,4 @@ if (!is_null($events['events'])) {
 } 
 echo "OK";
 
-function getdata(intervalTime = 10){
-            setInterval(function(){ 
-                fetch('http://192.168.10.241:5000/api/fromDB')
-                .then(response => response.json())
-                .then(data => {
-                console.log(data) // Prints result from `response.json()` in getRequest
-                var namesym = document.getElementById($event['message']['text']).value;
-                //var check_data = 0;
-                // alert(namesym);
-                for(var i=0;i<100;i++){
-                    if(namesym == data.DB[i].name || namesym == data.DB[i].symbol){
-                    $USDprice = data.DB[i].quotes.USD.price;
-                    $THBprice = data.DB[i].quotes.THB.price;
-                    $respMessage = 'Price:  '.$USDprice.'USD, \n'.$THBprice.' THB';
-                    }
-                }
-                })
-                .catch(error => console.error(error))
-            }, intervalTime);
-        }
+?>
