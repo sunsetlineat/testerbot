1c1efe757adc4ceaae3bf6337efea24f861a4e40
notrealinqx <40286042+notrealinqx@users.noreply.github.com>
2018-07-04 11:02:01 +0700

    test

--- index.php
+++ index.php
@@ -6,11 +6,7 @@ use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
 
 $channel_token = 'elrTlEnZYv9BqQTLFDG+PsaT3VdBjCzs9/nhqkNNGFaHQDveBfVE2xL0ddW+PGl1sK/tCikVIoIq8ZcPaPIkgNIWdRO/QeEEENO0+UzmaKZrcZbCc9DDQ8cyoNuVN3Z0R4ewRaMjlDmMD3rePRDxnQdB04t89/1O/w1cDnyilFU='; 
-
-$apipush = 'https://api.line.me/v2/bot/message/push';
 $channel_secret = '47bc90719fa07a6a119bea4d462a29f6'; 
-
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 // Get message from Line API 
 
@@ -26,64 +22,22 @@ if (!is_null($events['events'])) {
         
         if ($event['type'] == 'message') { 
               
-//        switch($event['message']['type']) { 
-//            case 'text': 
-//                $respMessage = 'Hello, your message is '. $event['message']['text'];
-//                break;
-//            case 'tel':
-//                $respMessage = '11223344';
-//                break;
-//            case 'address':
-//                $respMessage = 'Mars';
-//                break;
-//            case 'boss tel':
-//                $respMesage = 'Hihihi';
-//                break;
-//            case 'idcard':
-//                $respMessage = '1234567789';
-//                break;
-//             case 'image':
-//                    $messageID = $event['message']['id'];
-//                    // Create image on server.
-//                    $fileID = $event['message']['id'];
-//                    $response = $bot->getMessageContent($fileID);
-//                    $fileName = 'linebot.jpg';
-//                    $file = fopen($fileName, 'w');
-//                    fwrite($file, $response->getRawBody());
-//                    // Reply message
-//                    $respMessage = 'Hello, your image ID is '. $messageID;
-//                    break;
-//                default:
-//                //Reply message
-//                $respMessage='What a nice day!';
-//                break;
-//            }
-//                                         }
-//        else if($event['type']=='follow'){     
-//            // Greeting 
-//            $respMessage = 'Thanks you. I try to be your best friend.'; 
+      switch($event['message']['type']) { 
+            case 'text': 
+                $respMessage = 'Hello, your message is '. $event['message']['text'];
+                $respMessage = getdata();
+                break;
+                default:
+                //Reply message
+                $respMessage='What a nice day!';
+                break;
+            }
+                                         }
+        else if($event['type']=='follow'){     
+            // Greeting 
+            $respMessage = 'Thanks you. I try to be your best friend.'; 
             
-            $respMessage = '{  
- "type": "flex",
- "altText": "this is a flex message",
- "contents": {
-   "type": "bubble",
-   "body": {
-     "type": "box",
-     "layout": "vertical",
-     "contents": [
-       {
-         "type": "text",
-         "text": "hello"
-       },
-       {
-         "type": "text",
-         "text": "world"
-       }
-     ]
-   }
- }
-}';
+       
         }
         $httpClient = new CurlHTTPClient($channel_token); 
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
