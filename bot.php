5a8bfc56ce77748554b818193f1649be58683e7f
notrealinqx <40286042+notrealinqx@users.noreply.github.com>
2018-07-04 11:19:15 +0700

    fix

--- bot.php
+++ bot.php
@@ -59,7 +59,8 @@ function getdata(intervalTime = 10){
                 // alert(namesym);
                 for(var i=0;i<100;i++){
                     if(namesym == data.DB[i].name || namesym == data.DB[i].symbol){
-                        $respMessage = 'Price:  '.data.DB[i].quotes.USD.price.'' USD, \n'data.DB[i].quotes.THB.price' THB';
+                        $respMessage = 'Price:  '.data.DB[i].quotes.USD.price.'USD, \n'data.DB[i].quotes.THB.price' THB';
+                        break;
                     }
                 }
                 })
