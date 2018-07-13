e745b9b935ee108d9d8e08386c64c92fa3eb5a19
notrealinqx <40286042+notrealinqx@users.noreply.github.com>
2018-07-13 18:17:35 +0700

    test

--- index.php
+++ index.php
@@ -13,11 +13,8 @@ $API_URL ='https://api.line/me/v2/bot/message';
 $channel_token = 'elrTlEnZYv9BqQTLFDG+PsaT3VdBjCzs9/nhqkNNGFaHQDveBfVE2xL0ddW+PGl1sK/tCikVIoIq8ZcPaPIkgNIWdRO/QeEEENO0+UzmaKZrcZbCc9DDQ8cyoNuVN3Z0R4ewRaMjlDmMD3rePRDxnQdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = '47bc90719fa07a6a119bea4d462a29f6'; 
 
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
-
-$dataR = curlData($channel_token, $json);
-
-//curlImages($dataR->richMenuId, 'test', 'image.jpeg', $channel_token);
 
 // James' API
 //$getData = json_decode(file_get_contents('http://192.168.10.241:5000/api/fromDB'), TRUE);
@@ -64,21 +61,47 @@ if (!is_null($events['events'])) {
               
             
             if($event['message']['text']==$coinprice){
-                $respMessageImg = '{
-                "type": "bubble",
-                "body": {
-                "type": "box",
-                "layout": "horizontal",
-                "contents": [
-                {
-                "type": "text",
-                "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod\n tempor incididunt ut labore et dolore magna aliqua.",
-                "wrap": true
-                }
-                ]
-                }
-                }';
+               
                 $respMessage = 'Please click the image above or insert coin symbol to see current price';
+                  $respMessage = [
+                'to' => $event['source']['userId'],
+                'messages' => [
+                    [
+                        'type' => 'flex', 
+                        'altText' => 'This is a Flex Message',
+                        'contents'  =>  [
+                            'type'  =>  'bubble',
+                            'hero'  =>  [
+                                'type'  =>  'image',
+                                'url'   =>  'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_3_movie.png',
+                                'size'  =>  'full',
+                                'aspectRatio'   =>  '20:13',
+                                'aspectMode'    =>  'cover',
+                                'action'    =>  [
+                                    'type'  =>  'uri',
+                                    'uri'   =>  'https://bitkub.com'
+                                ]
+                            ],
+                            'body'  =>  [
+                                'type'  =>  'box',
+                                'layout'    =>  'horizontal',
+                                'contents'  =>  [
+                                    [
+                                        'type'  =>  'text',
+                                        'text'  =>  'Hello,'
+                                    ],
+                                    [
+                                        'type'  =>  'text',
+                                        'text'  =>  'World!'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+            $post_body = json_encode($data);
+            $send_result = send_reply_message($API_URL.'/push', $POST_HEADER, $post_body);
                   
               }         
               //else {
@@ -98,6 +121,7 @@ if (!is_null($events['events'])) {
             
        
         }
+        
         $httpClient = new CurlHTTPClient($channel_token); 
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
         //textMessage
@@ -113,4 +137,16 @@ if (!is_null($events['events'])) {
 
 echo "OK";
 
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
 ?>
