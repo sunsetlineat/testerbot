0a0a1e65f5bfeed56a5f6355d7edfe2c00876bc7
notrealinqx <40286042+notrealinqx@users.noreply.github.com>
2018-07-16 10:07:38 +0700

    TTTTTTTTTTTTT

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (!is_null($request_array['events'])) {
             $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
             $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
-                    $temp = $text.' -> '.$priceList[strtoupper($text)];
+                    $temp = $priceList[strtoupper($text)];
                     $data = [
                         'to' => $event['source']['userId'],
                         'message' => [['type' => 'text', 'text' => json_encode($temp)]] 
