8edb73292ec4c4125c3cf402435a2adca4f11f0b
notrealinqx <40286042+notrealinqx@users.noreply.github.com>
2018-07-23 14:25:38 +0700

    update

--- index.php
+++ index.php
@@ -84,6 +84,16 @@ if ( sizeof($request_array['events']) > 0 ) {
 
                         }elseif($text=='EVERY 30 MINUTES'){
 
+                                //test
+
+                                 $data = [
+                                'replyToken' => $reply_token,
+                // 'messages' => [['type' => 'text', 'text' => $reply_message]]
+                                'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+                                ];
+                                $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+                                $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
                                 $params = array (
                                 'user_id'    =>  $event['source']['userID'],
                                 'every_min'  =>  '30',
