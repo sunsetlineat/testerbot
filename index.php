d91bf0bf83dd1781b3e2ad662205ebeb7cf7f018
sunsetlineat <41568785+sunsetlineat@users.noreply.github.com>
2018-07-25 11:50:18 +0700

    index.php
    update code

--- index.php
+++ index.php
@@ -34,13 +34,10 @@ if(!empty($getData['data'])){
     $priceList[$val['symbol']] = 'Current Price: ' . $val['quotes']['USD']['price'] . ' USD '; 
     }
 }
-
 if ( sizeof($request_array['events']) > 0 ) {
     foreach ($request_array['events'] as $event) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
-
-
         if ( $event['type'] == 'message' ) {
             if( $event['message']['type'] == 'text' ) {
                 // Type Text
@@ -54,18 +51,20 @@ if ( sizeof($request_array['events']) > 0 ) {
                     ];
                     $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
                     $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-                } elseif($text == 'Contact us'){
+                } elseif($text=='Bitkub Menu'){
+                    //bitkubMenu
+                    $data = '{"to":"'. $event['source']['userId'] .'","messages":[{"type":"flex","altText":"This is a Flex Message","contents":{"type":"carousel","contents":[{"type":"bubble","hero":{"type":"image","url":"https://bitkubblockchain.com/wp-content/uploads/2018/01/line-menu-02.jpg","size":"full","aspectRatio":"20:13","aspectMode":"cover"},"body":{"type":"box","layout":"vertical","spacing":"md","contents":[{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"text","text":"CLICK TO CHECK CURRENT PRICE","weight":"bold","color":"#1DB446","size":"sm"}]},{"type":"separator","margin":"lg"},{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"box","layout":"horizontal","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"BTC","displayText":"Bitcoin","data":"BTC"}},{"type":"button","style":"primary","action":{"type":"postback","label":"ETH","displayText":"ETH","data":"ETH"}},{"type":"button","style":"primary","action":{"type":"postback","label":"WAN","displayText":"WAN","data":"WAN"}}]},{"type":"box","layout":"horizontal","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"ADA","displayText":"ADA","data":"ADA"}},{"type":"button","style":"primary","action":{"type":"postback","label":"OMG","displayText":"OMG","data":"OMG"}},{"type":"button","style":"primary","action":{"type":"postback","label":"XRP","displayText":"XRP","data":"XRP"}}]}]}]},"footer":{"type":"box","layout":"vertical","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"SET NOTIFICATION TIME","displayText":"SET NOTIFICATION TIME","data":"setNotiTime"}},{"type":"button","style":"secondary","margin":"sm","action":{"type":"uri","label":"CHECK OUT BITKUB MARKET","uri":"https://www.bitkub.com/market"}}]}},{"type":"bubble","body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"Bitkub Online Co., Ltd","weight":"bold","size":"xl"},{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"box","layout":"baseline","spacing":"sm","contents":[{"type":"text","text":"Place","color":"#aaaaaa","size":"sm","flex":1},{"type":"text","text":"15cd, 15th floor, 29/1, Piya Place Building, Pathum Wan, Bangkok","wrap":true,"color":"#666666","size":"sm","flex":5}]},{"type":"box","layout":"baseline","spacing":"sm","contents":[{"type":"text","text":"E-mail","color":"#aaaaaa","size":"sm","flex":1},{"type":"text","text":"support@bitkub.com","wrap":true,"color":"#666666","size":"sm","flex":5}]}]}]},"footer":{"type":"box","layout":"vertical","spacing":"sm","contents":[{"type":"button","style":"link","height":"sm","action":{"type":"uri","label":"CALL","uri":"tel:0203229555"}}],"flex":2}},{"type":"bubble","body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"SET NOTIFICATION TIME","weight":"bold","color":"#1DB446","size":"sm"}]},"footer":{"type":"box","layout":"vertical","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"EVERY 30 MINUTES","displayText":"EVERY 30 MINUTES","data":"EVERY 30 MINUTES"}},{"type":"button","style":"primary","action":{"type":"postback","label":"EVERY 1 HOUR","displayText":"EVERY 1 HOUR","data":"EVERY 1 HOUR"}},{"type":"button","style":"primary","action":{"type":"postback","label":"EVERY DAY","displayText":"EVERY DAY","data":"EVERY DAY"}},{"type":"spacer","size":"sm"}],"flex":0}}]}}]}';
+                     $post_body = $data;
+                     $send_result =send_reply_message($API_URL.'/push',$POST_HEADER,$post_body);
+                }
+                elseif($text == 'Contact us'){
                      $data = '{"to":"'. $event['source']['userId'] .'","messages":[{"type":"flex","altText":"This is a Flex Message","contents":{"type":"bubble","body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"Bitkub Online Co., Ltd","weight":"bold","size":"xl"},{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"box","layout":"baseline","spacing":"sm","contents":[{"type":"text","text":"Place","color":"#aaaaaa","size":"sm","flex":1},{"type":"text","text":"15cd, 15th floor, 29/1, Piya Place Building, Pathum Wan, Bangkok","wrap":true,"color":"#666666","size":"sm","flex":5}]},{"type":"box","layout":"baseline","spacing":"sm","contents":[{"type":"text","text":"E-mail","color":"#aaaaaa","size":"sm","flex":1},{"type":"text","text":"support@bitkub.com","wrap":true,"color":"#666666","size":"sm","flex":5}]}]}]},"footer":{"type":"box","layout":"vertical","spacing":"sm","contents":[{"type":"button","style":"link","height":"sm","action":{"type":"uri","label":"CALL","uri":"tel:0203229555"}},{"type":"spacer","size":"sm"}],"flex":0}}}]}';
                      $post_body = $data;
                      $send_result =send_reply_message($API_URL.'/push',$POST_HEADER,$post_body);
-
-
                 } elseif($text == 'Coin Price') {
-                    $data = '{"to":"'. $event['source']['userId'] .'","messages":[{"type":"flex","altText":"This is a Flex Message","contents":{"type":"bubble","hero":{"type":"image","url":"https://bitkubblockchain.com/wp-content/uploads/2018/01/line-menu-test.png","size":"full","aspectRatio":"20:13","aspectMode":"cover"},"body":{"type":"box","layout":"vertical","spacing":"md","contents":[{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"text","text":"CLICK TO CHECK CURRENT PRICE","weight":"bold","color":"#1DB446","size":"sm"}]},{"type":"separator","margin":"lg"},{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"box","layout":"horizontal","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"BTC","displayText":"Bitcoin","data":"BTC"}},{"type":"button","style":"primary","action":{"type":"postback","label":"ETH","displayText":"ETH","data":"ETH"}},{"type":"button","style":"primary","action":{"type":"postback","label":"WAN","displayText":"WAN","data":"WAN"}}]},{"type":"box","layout":"horizontal","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"ADA","displayText":"ADA","data":"ADA"}},{"type":"button","style":"primary","action":{"type":"postback","label":"OMG","displayText":"OMG","data":"OMG"}},{"type":"button","style":"primary","action":{"type":"postback","label":"XRP","displayText":"XRP","data":"XRP"}}]}]}]},"footer":{"type":"box","layout":"vertical","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"SET NOTIFICATION TIME","displayText":"SET NOTIFICATION TIME","data":"SET NOTIFICATION TIME"}},{"type":"button","style":"secondary","margin":"sm","action":{"type":"uri","label":"CHECK OUT BITKUB MARKET","uri":"https://www.bitkub.com/market"}}]}}}]}';
+                    $data = '{"to":"'. $event['source']['userId'] .'","messages":[{"type":"flex","altText":"This is a Flex Message","contents":{"type":"bubble","hero":{"type":"image","url":"https://bitkubblockchain.com/wp-content/uploads/2018/01/line-menu-02.jpg","size":"full","aspectRatio":"20:13","aspectMode":"cover"},"body":{"type":"box","layout":"vertical","spacing":"md","contents":[{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"text","text":"CLICK TO CHECK CURRENT PRICE","weight":"bold","color":"#1DB446","size":"sm"}]},{"type":"separator","margin":"lg"},{"type":"box","layout":"vertical","margin":"lg","spacing":"sm","contents":[{"type":"box","layout":"horizontal","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"BTC","displayText":"Bitcoin","data":"BTC"}},{"type":"button","style":"primary","action":{"type":"postback","label":"ETH","displayText":"ETH","data":"ETH"}},{"type":"button","style":"primary","action":{"type":"postback","label":"WAN","displayText":"WAN","data":"WAN"}}]},{"type":"box","layout":"horizontal","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"ADA","displayText":"ADA","data":"ADA"}},{"type":"button","style":"primary","action":{"type":"postback","label":"OMG","displayText":"OMG","data":"OMG"}},{"type":"button","style":"primary","action":{"type":"postback","label":"XRP","displayText":"XRP","data":"XRP"}}]}]}]},"footer":{"type":"box","layout":"vertical","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"SET NOTIFICATION TIME","displayText":"SET NOTIFICATION TIME","data":"SET NOTIFICATION TIME"}},{"type":"button","style":"secondary","margin":"sm","action":{"type":"uri","label":"CHECK OUT BITKUB MARKET","uri":"https://www.bitkub.com/market"}}]}}}]}';
                     $post_body = $data;
                     $send_result = send_reply_message($API_URL.'/push', $POST_HEADER, $post_body);
-
                 }
             } else { 
               
@@ -79,19 +78,54 @@ if ( sizeof($request_array['events']) > 0 ) {
                                 'replyToken' => $reply_token,
                                 'messages' => [[ 'type' => 'text', 'text' => $temp ]]
                             ];
-                        }elseif($text=='SET NOTIFICATION TIME'){
-
+                        
+                        } elseif($text=='SET NOTIFICATION TIME'){
                             $data = '{"to":"'. $event['source']['userId'] .'","messages":[{"type":"flex","altText":"This is a Flex Message","contents":{"type":"bubble","body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"SET NOTIFICATION TIME","weight":"bold","color":"#1DB446","size":"sm"}]},"footer":{"type":"box","layout":"vertical","spacing":"sm","contents":[{"type":"button","style":"primary","action":{"type":"postback","label":"EVERY 30 MINUTES","displayText":"EVERY 30 MINUTES","data":"EVERY 30 MINUTES"}},{"type":"button","style":"primary","action":{"type":"postback","label":"EVERY 1 HOUR","displayText":"EVERY 1 HOUR","data":"EVERY 1 HOUR"}},{"type":"button","style":"primary","action":{"type":"postback","label":"EVERY DAY","displayText":"EVERY DAY","data":"EVERY DAY"}},{"type":"spacer","size":"sm"}],"flex":0}}}]}';
                             $post_body = $data;
                             $send_result = send_reply_message($API_URL.'/push', $POST_HEADER, $post_body);
-
-                        }else {
+                        }
+                        elseif($text=='EVERY 30 MINUTES'){
+                                // $params = array (
+                                // 'user_id'    =>  $event['source']['userID'],
+                                // 'every_min'  =>  '30',
+                                // 'create_at' =>date('Y-m-d'),
+                                // );
+                                // $statement = $connection->prepare('INSERT INTO public.notification(user_id,every_min,create_at) VALUES (:user_id, :every_min, :create_at)');
+                                // $result = $statement->execute($params);
+                           $data = [
+                                'replyToken' => $reply_token,
+                                'messages' => [[ 'type' => 'text', 'text' => 'Set notification time : every 30 minutes' ]]
+                            ];   
+                        }elseif($text=='EVERY 1 HOUR'){
+                                // $params = array (
+                                // 'user_id'    =>  $event['source']['userID'],
+                                // 'every_min'  =>  '60',
+                                // 'create_at' =>date('Y-m-d'),
+                                // );
+                                // $statement = $connection->prepare('INSERT INTO public.notification(user_id,every_min,create_at) VALUES (:user_id, :every_min, :create_at)');
+                                // $result = $statement->execute($params);
+                            $data = [
+                                'replyToken' => $reply_token,
+                                'messages' => [[ 'type' => 'text', 'text' => 'Set notification time : every 1 hour' ]]
+                            ];
+                        }elseif($text=='EVERY DAY'){
+                                // $params = array (
+                                // 'user_id'    =>  $event['source']['userID'],
+                                // 'every_min'  =>  '1440',
+                                // 'create_at' =>date('Y-m-d'),
+                                // );
+                                // $statement = $connection->prepare('INSERT INTO public.notification(user_id,every_min,create_at) VALUES (:user_id, :every_min, :create_at)');
+                                // $result = $statement->execute($params);
+                            $data = [
+                                'replyToken' => $reply_token,
+                                'messages' => [[ 'type' => 'text', 'text' => 'Set notification time : every day' ]]
+                            ];
+                        } else {
                             $data = [
                                 'replyToken' => $reply_token,
                                 'messages' => [[ 'type' => 'text', 'text' => 'Not Found' ]]
                             ];
                 }
-
             $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
             $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
             
@@ -100,8 +134,14 @@ if ( sizeof($request_array['events']) > 0 ) {
             }
     }
 }
+// notify
+    // for($x=0;$x<=1800;$x++){
+    // }
+    // for($x=0;$x<=3600;$x++){
+    // }
+    // for($x=0;$x<=86400;$x++){
+    // }
 echo "OK";
-
 function send_reply_message($url, $post_header, $post_body)
 {
     $ch = curl_init($url);
